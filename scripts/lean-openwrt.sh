ccd1dba37b42eaabce9442b7e1660bed14503da8
Helmi Amirudin <helmilaw@gmail.com>
2021-09-15 00:07:45 +0700

    If you use some codes from here, please give credit to @helmiau
    Add Xradio kernel for Orange Pi Zero

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -193,6 +193,10 @@ wget --no-check-certificate $(curl -L https://github.com/helmiau/helmiau/release
 mkdir -p package/base-files/files/etc/v2raya
 git clone http://github.com/v2rayA/v2raya-web package/base-files/files/etc/v2raya/web
 
+# Add Xradio kernel for Orange Pi Zero
+svn co https://github.com/melsem/openwrt-lede_xradio-xr819_soc-audio/trunk/xradio-Openwrt_kernel-5.4.xx
+mv xradio-Openwrt_kernel-5.4.xx xradio
+
 #-----------------------------------------------------------------------------
 #   End of @helmiau terminal scripts additionals menu
 #-----------------------------------------------------------------------------
