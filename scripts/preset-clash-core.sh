54544b118a0f374d1e46431754d76aeb52c01f25
Helmi Amirudin <helmilaw@gmail.com>
2022-01-09 10:47:28 +0700

    If you use some codes from here, please give credit to @helmiau
    clash-core: use Meta kernel for TUN mode

--- scripts/preset-clash-core.sh
+++ scripts/preset-clash-core.sh
@@ -3,31 +3,57 @@
 # File name: preset-clash-core.sh
 # Usage: <preset-clash-core.sh $platform> | example: <preset-clash-core.sh armv8>
 # System Required: Linux
-# Version: 1.0
+# Version: 3.0
 # Lisence: MIT
 # Author: SuLingGG, Helmi Amirudin
-# Blog: https://mlapp.cn
+# Blog: https://helmiau.com
+# Description: This script will install clash core kernel from Dreamacro or Meta or Vernesong
+#=================================================
+# Select core	: Remove # symbol from first word of wget line
+# Unselect core	: Add # symbol at first word from wget line
 #=================================================
 
-mkdir -p files/etc/openclash/core
+# Initial recognition
+COREDIR="files/etc/openclash/core"
+APIGIT="https://api.github.com/repos"
+mkdir -p $COREDIR
 
-open_clash_premium_url=$(curl -sL https://api.github.com/repos/Dreamacro/clash/releases/tags/premium | grep /clash-linux-$1 | awk -F '"' '{print $4}')
-open_clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-$1 | awk -F '"' '{print $4}')
-# offical_clash_main_url=$(curl -sL https://api.github.com/repos/Dreamacro/clash/releases/tags/v1.3.5 | grep /clash-linux-$1 | awk -F '"' '{print $4}')
-clash_tun_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN-Premium | grep /clash-linux-$1 | awk -F '"' '{print $4}')
-clash_game_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN | grep /clash-linux-$1 | awk -F '"' '{print $4}')
+# TEST debug
+# curl -sL https://api.github.com/repos/Dreamacro/clash/releases | grep /clash-linux-armv8 | awk -F '"' '{print $4}' | awk 'NR==1 {print; exit}'
 
-wget --show-progress -qO- $open_clash_premium_url | gunzip -c > files/etc/openclash/core/clash
-wget --show-progress -qO- $open_clash_main_url | tar xOvz > files/etc/openclash/core/clash_original
-# wget --show-progress -qO- $offical_clash_main_url | gunzip -c > files/etc/openclash/core/clash
-wget --show-progress -qO- $clash_tun_url | gunzip -c > files/etc/openclash/core/clash_tun
-wget --show-progress -qO- $clash_game_url | tar xOvz > files/etc/openclash/core/clash_game
+# Dreamacro Core has:
+# - original core
+# - tun premium core
+# Please use tun game core from Meta or Vernesong core
+clash_url=$(curl -sL $APIGIT/Dreamacro/clash/releases | grep /clash-linux-$1 | awk -F '"' '{print $4}' | sed -n '1p')
+clash_tun_url=$(curl -sL $APIGIT/Dreamacro/clash/releases/tags/premium | grep /clash-linux-$1 | awk -F '"' '{print $4}')
+#wget --show-progress -qO- $clash_url | gunzip -c > $COREDIR/clash
+#wget --show-progress -qO- $clash_tun_url | gunzip -c > $COREDIR/clash_tun
 
-chmod +x files/etc/openclash/core/clash*
+# Clash Meta Core has:
+# - tun premium core
+# Please use original and tun game core from Dreamacro or Vernesong core
+# Docs: https://github.com/MetaCubeX/Clash.Meta/tree/Dev
+clash_tun_url=$(curl -sL $APIGIT/MetaCubeX/Clash.Meta/releases | grep /Clash.Meta-linux-$1 | awk -F '"' '{print $4}' | sed -n '1p')
+wget --show-progress -qO- $clash_tun_url | gunzip -c > $COREDIR/clash_tun
+
+# Vernesong Core has:
+# - original core
+# - tun premium core
+# - tun game core
+clash_url=$(curl -sL $APIGIT/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-$1 | awk -F '"' '{print $4}')
+clash_tun_url=$(curl -sL $APIGIT/vernesong/OpenClash/releases/tags/TUN-Premium | grep /clash-linux-$1 | awk -F '"' '{print $4}')
+clash_game_url=$(curl -sL $APIGIT/vernesong/OpenClash/releases/tags/TUN | grep /clash-linux-$1 | awk -F '"' '{print $4}')
+wget --show-progress -qO- $clash_url | tar xOvz > $COREDIR/clash
+#wget --show-progress -qO- $clash_tun_url | gunzip -c > $COREDIR/clash_tun
+wget --show-progress -qO- $clash_game_url | tar xOvz > $COREDIR/clash_game
+
+chmod +x $COREDIR/clash*
 
 # Offline images sources
-mkdir -p files/www/luci-static/resources/openclash
-wget --show-progress -qO files/www/luci-static/resources/openclash/Tutorials.svg https://img.shields.io/badge/Tutorials--lightgrey?logo=Wikipedia&style=social
-wget --show-progress -qO files/www/luci-static/resources/openclash/Star.svg https://img.shields.io/badge/Star--lightgrey?logo=github&style=social
-wget --show-progress -qO files/www/luci-static/resources/openclash/Telegram.svg https://img.shields.io/badge/Telegram--lightgrey?logo=Telegram&style=social
-wget --show-progress -qO files/www/luci-static/resources/openclash/Sponsor.svg https://img.shields.io/badge/Sponsor--lightgrey?logo=ko-fi&style=social
+YACD="files/www/luci-static/resources/openclash"
+mkdir -p $YACD
+wget --show-progress -qO $YACD/Tutorials.svg https://img.shields.io/badge/Tutorials--lightgrey?logo=Wikipedia&style=social
+wget --show-progress -qO $YACD/Star.svg https://img.shields.io/badge/Star--lightgrey?logo=github&style=social
+wget --show-progress -qO $YACD/Telegram.svg https://img.shields.io/badge/Telegram--lightgrey?logo=Telegram&style=social
+wget --show-progress -qO $YACD/Sponsor.svg https://img.shields.io/badge/Sponsor--lightgrey?logo=ko-fi&style=social
