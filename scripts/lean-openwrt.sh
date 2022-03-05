74bc794f3090e8763e3fc98f6ad73dace797c9ba
Helmi Amirudin <helmilaw@gmail.com>
2022-03-06 00:07:14 +0700

    If you use some codes from here, please give credit to @helmiau

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -30,6 +30,7 @@ pushd package/community
 
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+[ ! -d openwrt-passwall/luci-app-passwall ] && svn co https://github.com/NueXini/NueXini_Packages/branches/main/luci-app-passwall openwrt-passwall/luci-app-passwall
 sed -i 's/ upx\/host//g' openwrt-passwall/v2ray-plugin/Makefile
 grep -lr upx/host openwrt-passwall/* | xargs -t -I {} sed -i '/upx\/host/d' {}
 
